dc3e291a82612812f7dcd652aee2d7e62c8af923
João Pedro Castro <sirjpcastro@gmail.com>
2024-06-06 12:48:44 -0300

    feat: cria um modelo de usuário customizado

--- api/models.py
+++ api/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 import uuid
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
 
 class Tarefas(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
